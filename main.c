75616684d5581bf708d310e171252088439a50b3
ck88889 <95720418+ck88889@users.noreply.github.com>
2022-11-16 13:11:21 -0500

    restore to when code was working

--- main.c
+++ main.c
@@ -21,9 +21,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-#include "stm32f4xx.h"
-#include <stdio.h>
-#include "sds011.h"
+#include "stm32f4xx_hal.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -46,237 +44,20 @@ I2C_HandleTypeDef hi2c1;
 UART_HandleTypeDef huart2;
 
 /* USER CODE BEGIN PV */
-SDS sds;
 
-uint8_t data[50];
-uint16_t size = 0;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
-static void MX_I2C1_Init(void);
 static void MX_USART2_UART_Init(void);
+static void MX_I2C1_Init(void);
 /* USER CODE BEGIN PFP */
-#ifdef __GNUC__
-#define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
-#define GETCHAR_PROTOTYPE int __io_getchar(void)
-#else
-#define PUTCHAR_PROTOTYPE int fputc(int ch, FILE *f)
-#define GETCHAR_PROTOTYPE int fgetc(FILE *f)
-#endif
 
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-extern I2C_HandleTypeDef hi2c1;
-
-uint8_t dpRows;
-uint8_t dpBacklight;
-uint8_t dpFunction;
-uint8_t dpControl;
-uint8_t dpMode;
-
-uint8_t special1[8] = {
-        0b00000,
-        0b11001,
-        0b11011,
-        0b00110,
-        0b01100,
-        0b11011,
-        0b10011,
-        0b00000
-};
-
-uint8_t special2[8] = {
-        0b11000,
-        0b11000,
-        0b00110,
-        0b01001,
-        0b01000,
-        0b01001,
-        0b00110,
-        0b00000
-};
-
-static void ExpanderWrite(uint8_t _data);
-
-static void DelayInit(void)
-{
-  CoreDebug->DEMCR &= ~CoreDebug_DEMCR_TRCENA_Msk;
-  CoreDebug->DEMCR |=  CoreDebug_DEMCR_TRCENA_Msk;
-
-  DWT->CTRL &= ~DWT_CTRL_CYCCNTENA_Msk; //~0x00000001;
-  DWT->CTRL |=  DWT_CTRL_CYCCNTENA_Msk; //0x00000001;
-
-  DWT->CYCCNT = 0;
-
-  /* 3 NO OPERATION instructions */
-  __ASM volatile ("NOP");
-  __ASM volatile ("NOP");
-  __ASM volatile ("NOP");
-}
-
-static void PulseEnable(uint8_t _data)
-{
-  const ENABLE = 0x04;
-  ExpanderWrite(_data | ENABLE);
-  DelayUS(20);
-
-  ExpanderWrite(_data & ~ENABLE);
-  DelayUS(20);
-}
-
-void init_LCD(uint8_t rows){
-	dpRows = rows;
-
-	dpBacklight = 0x08; //backlight
-
-	dpFunction = 0x00 | 0x00 | 0x00;  // LCD_4BITMODE | LCD_1LINE | LCD_5x8DOTS
-
-	 if (dpRows > 1){
-	    dpFunction |= 0x08; //LCD_2LINE
-	 }else{
-	    dpFunction |= 0x04; //LCD_5x10DOTS
-	 }
-
-	 DelayInit();//initialize SysTimer
-	 HAL_Delay(50);
-
-	 ExpanderWrite(dpBacklight);
-	 HAL_Delay(1000);
-
-	 //4-bit mode
-	 write_bit(0x03 << 4);
-	 DelayUS(4500);
-
-	  write_bit(0x03 << 4);
-	  DelayUS(4500);
-
-	  write_bit(0x03 << 4);
-	  DelayUS(4500);
-
-	  write_bit(0x02 << 4);
-	  DelayUS(100);
-
-	  //display control
-	  SendCommand(0x20 | dpFunction); //LCD_FUNCTIONSET | dpFunction
-
-	  dpControl = 0x04 | 0x00 | 0x00; //LCD_DISPLAYON | LCD_CURSOROFF | LCD_BLINKOFF
-	  display_LCD();
-	  clear_LCD();
-
-	  //display mode
-	  dpMode = 0x04 |0x00; //entry mode set, LCD_ENTRYSHIFTDECREMENT
-	  SendCommand(0x04| dpMode); //entry mode set
-	  DelayUS(4500);
-
-	  create_spec_char(0, special1);
-	  create_spec_char(1, special2);
-
-	  Home_LCD();
-
-}
-
-void set_cursor(uint8_t col, uint8_t row){
-	int row_offsets[] = { 0x00, 0x40, 0x14, 0x54 };
-	if (row >= dpRows){
-	   row = dpRows-1;
-	}
-
-	SendCommand(0x80 | (col + row_offsets[row])); //LCD_SETDDRAMADDR
-}
-
-void printstr_LCD(const char c[]){
-  while(*c) SendChar(*c++);
-}
-
-void SendChar(uint8_t ch){
-  Send(ch, 0x01); //registor select bit
-}
-
-void display_LCD(){
-	dpControl = 0x04; //display on
-	SendCommand(0x08 | dpControl); //LCD_DISPLAYCONTROL | dpControl
-}
-
-void no_display_LCD(){
-  dpControl &= ~0x04; //display on
-  SendCommand(0x08 | dpControl); //display control
-}
-
-void clear_LCD(){
-	SendCommand(0x01); //clear display
-	DelayUS(2000);
-}
-
-void create_spec_char(uint8_t location, uint8_t charmap[]){
-	location &= 0x7;
-	SendCommand(0x40| (location << 3));
-	for (int i=0; i<8; i++){
-		SendChar(charmap[i]);
-	}
-}
-
-void Home_LCD(){
-  SendCommand(0x02); //return home
-  DelayUS(2000);
-}
-
-void write_bit(uint8_t x){
-	ExpanderWrite(x);
-	PulseEnable(x);
-}
-
-void DelayUS(uint32_t us) {
-  uint32_t cycles = (SystemCoreClock/1000000L)*us;
-  uint32_t start = DWT->CYCCNT;
-  volatile uint32_t cnt;
-
-  do
-  {
-    cnt = DWT->CYCCNT - start;
-  } while(cnt < cycles);
-}
-
-void SendCommand(uint8_t cmd)
-{
-  Send(cmd, 0);
-}
-
-void Send(uint8_t value, uint8_t mode)
-{
-  uint8_t highnib = value & 0xF0;
-  uint8_t lownib = (value<<4) & 0xF0;
-  write_bit((highnib)|mode);
-  write_bit((lownib)|mode);
-}
-
-static void ExpanderWrite(uint8_t _data){
-  uint8_t data = _data | dpBacklight;
-  HAL_I2C_Master_Transmit(&hi2c1, (0x3F << 1), (uint8_t*)&data, 1, 10);
-  return 0;
-}
-
-void cursor(){
-  dpControl |= 0x02;
-  SendCommand(0x08 | dpControl);
-}
-
-void print_spec_ch(uint8_t index){
-  SendChar(index);
-}
-
-int _write(int file, char *ptr, int len)
- {
-	 int DataIdx;
-	 for (DataIdx = 0; DataIdx < len; DataIdx++)
-	 {
-		 ITM_SendChar(*ptr++);
-	 }
-	 return len;
- }
 
 /* USER CODE END 0 */
 
@@ -287,8 +68,6 @@ int _write(int file, char *ptr, int len)
 int main(void)
 {
   /* USER CODE BEGIN 1 */
-	//uint16_t readValue;
-	//float voltage;
 
   /* USER CODE END 1 */
 
@@ -310,57 +89,25 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
-  MX_I2C1_Init();
   MX_USART2_UART_Init();
+  MX_I2C1_Init();
   /* USER CODE BEGIN 2 */
-  setvbuf(stdin, NULL, _IONBF, 0);
 
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-
-  init_LCD(2);//initialize
-  clear_LCD();//clear buffer
-
-  no_display_LCD();
-  cursor();
-  set_cursor(0,0);
-  printstr_LCD("HELLO");
-  print_spec_ch(0);
-
-  display_LCD();//show characters
-
   while (1)
   {
-	  /*HAL_GPIO_TogglePin(GREEN_LED_GPIO_Port, GREEN_LED_Pin);
-	  HAL_Delay(5000); */
-
-	  //new code sigh
-
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-	 // uint8_t Test[] = "Hello world !!!\r\n";
-	 // HAL_UART_Transmit(&huart2, Test, sizeof(Test),10);
-	 // HAL_Delay(1000);
-
 	  //toggle LED light with blue button
-	  HAL_GPIO_TogglePin(GPIOD, GPIO_PIN_15);
-	  if(HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13)== GPIO_PIN_RESET){
-		  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET); //turn on
-	  }else{
-		  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET); //turn off
-	  }
-
-	  printf("Hello");
-
-	  size = sprintf(data, "%d %d \n\r", sdsGetPm2_5(&sds), sdsGetPm10(&sds));
-	  HAL_UART_Transmit_IT(&huart2, data, size);
-	  HAL_Delay(1000);
-
-
-
+	  	  if(HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13)== GPIO_PIN_RESET){
+	  		  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET); //turn on
+	  	  }else{
+	  		  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET); //turn off
+	  	  }
   }
   /* USER CODE END 3 */
 }
@@ -386,9 +133,9 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.HSEState = RCC_HSE_BYPASS;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-  RCC_OscInitStruct.PLL.PLLM = 8;
-  RCC_OscInitStruct.PLL.PLLN = 336;
-  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
+  RCC_OscInitStruct.PLL.PLLM = 4;
+  RCC_OscInitStruct.PLL.PLLN = 84;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
   RCC_OscInitStruct.PLL.PLLQ = 7;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
@@ -493,20 +240,20 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GREEN_LED_GPIO_Port, GREEN_LED_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
 
-  /*Configure GPIO pins : PC13 PC0 RX_Pin TX_Pin */
-  GPIO_InitStruct.Pin = GPIO_PIN_13|GPIO_PIN_0|RX_Pin|TX_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  /*Configure GPIO pin : B1_Pin */
+  GPIO_InitStruct.Pin = B1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+  HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
 
-  /*Configure GPIO pin : GREEN_LED_Pin */
-  GPIO_InitStruct.Pin = GREEN_LED_Pin;
+  /*Configure GPIO pin : LD2_Pin */
+  GPIO_InitStruct.Pin = LD2_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(GREEN_LED_GPIO_Port, &GPIO_InitStruct);
+  HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
 
 }
 
@@ -525,6 +272,12 @@ void Error_Handler(void)
   __disable_irq();
   while (1)
   {
+	  //toggle LED light with blue button
+	  	  if(HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_13)== GPIO_PIN_RESET){
+	  		  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET); //turn on
+	  	  }else{
+	  		  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET); //turn off
+	  	  }
   }
   /* USER CODE END Error_Handler_Debug */
 }
